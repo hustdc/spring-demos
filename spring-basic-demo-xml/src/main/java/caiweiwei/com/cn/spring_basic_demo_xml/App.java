e297f527279adf14b95031982a6d0cca333d2b39
hustdc <dcduanman@163.com>
2018-02-23 21:20:15 +0800

    add two empty maven projects

---
+++ spring-basic-demo-xml/src/main/java/caiweiwei/com/cn/spring_basic_demo_xml/App.java
@@ -0,0 +1,13 @@
+package caiweiwei.com.cn.spring_basic_demo_xml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
